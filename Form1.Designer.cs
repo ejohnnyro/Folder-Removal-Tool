ba59b10fe61d81176b3fd62e95b885117ba42410
Ioan Mihai <ioan11bl@gmail.com>
2020-09-23 12:50:34 +0300

    My first commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Folder_Removal_Tool
+{
+    partial class MainFRT
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFRT));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            resources.ApplyResources(this.listBox1, "listBox1");
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Name = "listBox1";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.ForeColor = System.Drawing.Color.Aqua;
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.ForeColor = System.Drawing.Color.Red;
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // MainFRT
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "MainFRT";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Folder_Removal_Tool
+{
+    public partial class MainFRT : Form
+    {
+        public MainFRT()
+        {
+            InitializeComponent();
+        }
+
+        public void addItemToListBox(string item)
+        {
+            if (this.listBox1.Items.Contains(item))
+            {
+                MessageBox.Show("Selected folder is already added to the list.");
+            }
+            
+            else
+            {
+                listBox1.Items.Add(item);
+            }
+           
+        }
+
+
+        public void removeItemFromList(string selectedItem)
+        {
+            listBox1.Items.Remove(selectedItem);
+        }
+
+        public void deleteAll()
+        {
+            string pathToDelete = listBox1.SelectedItem.ToString();
+            System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(pathToDelete);
+            // Delete this dir and all subdirs.
+            try
+            {
+                di.Delete(true);
+            }
+            catch (System.IO.IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            using FolderBrowserDialog fldrDlg = new FolderBrowserDialog();
+            if (fldrDlg.ShowDialog() == DialogResult.OK)
+            {
+                string selectedPath = fldrDlg.SelectedPath;
+                string ptf = selectedPath;
+               // System.Windows.Forms.MessageBox.Show(ptf);
+                addItemToListBox(ptf);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null)
+            {
+                System.Windows.Forms.MessageBox.Show("Select the folder that you want to remove from the list.");
+                return;
+            }
+            
+            else
+            {
+                string selectedItem = listBox1.SelectedItem.ToString();
+                removeItemFromList(selectedItem);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            deleteAll();
+        }
+    }
+}
